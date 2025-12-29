4df42ea9615a1d14035e2a73ba5ac0b53055170b
Aillet Gabriel <aillet.gabriel05@gmail.com>
2025-12-29 10:31:02 +0100

    delete constante

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,6 +4,7 @@ export const DEFAULT_OWNER = 'default'
 export const DEFAULT_ORIG_LANG_OWNER = 'en'
 export const IMPORTS_PATH = "burrito/ingredient/raw/_local_/_local_/%Project%?ipath=";
 export const IMPORTS_PATH_BATCH = "burrito/ingredients/raw/_local_/_local_/%Project%?ipath=";
+export const DELETE_PATH = "/burrito/ingredients/delete/%Project%?ipath="
 export const USER_RESOURCES_PATH = "burrito/ingredient/git.door43.org/uw/"
 export const EXIST_PATH = "burrito/paths/_local_/_local_/";
 export const BASE_URL = "";
